bf74a011e9b144d0f4062e82089a1427b5e20d4a
rui.cheng <rui.cheng@sympli.com.au>
2026-08-05 22:38:33 +1000

    Share an amount evenly between several ways

--- src/Ledger/Money.cs
+++ src/Ledger/Money.cs
@@ -5,4 +5,6 @@ public static class Money
     public static decimal Round(decimal amount) => Math.Round(amount, 2, MidpointRounding.ToEven);
 
     public static decimal Clamp(decimal amount, decimal ceiling) => Math.Min(amount, ceiling);
+
+    public static decimal Share(decimal amount, int ways) => Math.Round(amount / ways, 2, MidpointRounding.ToEven);
 }
