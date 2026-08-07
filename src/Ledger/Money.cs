08c801d36c9b020c61e96341cb1c254306ce69c7
rui.cheng <rui.cheng@sympli.com.au>
2026-08-07 20:59:02 +1000

    Rebate part of a charge at a given rate

--- src/Ledger/Money.cs
+++ src/Ledger/Money.cs
@@ -63,4 +63,6 @@ public static class Money
     public static decimal Surtax(decimal amount, decimal rate) => Round(amount * rate / 100m);
 
     public static decimal Bonus(decimal amount, decimal rate) => Round(amount * rate / 100m);
+
+    public static decimal Rebate(decimal amount, decimal rate) => Round(amount * rate / 100m);
 }
