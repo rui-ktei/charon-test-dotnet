ed87e9466bb7181c4747a1dc4c472a6fe45a7fe8
rui.cheng <rui.cheng@sympli.com.au>
2026-08-07 21:22:36 +1000

    Round a charge at a given rate

--- src/Ledger/Money.cs
+++ src/Ledger/Money.cs
@@ -67,4 +67,6 @@ public static class Money
     public static decimal Rebate(decimal amount, decimal rate) => Round(amount * rate / 100m);
 
     public static decimal Withholding(decimal amount, decimal rate) => Round(amount * rate / 100m);
+
+    public static decimal Rounding(decimal amount, decimal rate) => Round(amount * rate / 100m);
 }
