b7aee97079c27194fcea89ed7a362a31696ee5a6
rui.cheng <rui.cheng@sympli.com.au>
2026-08-10 13:29:15 +1000

    Post a charge at a given rate

--- tests/Ledger.Tests/MoneyTests.cs
+++ tests/Ledger.Tests/MoneyTests.cs
@@ -103,4 +103,10 @@ public class MoneyTests
     {
         Assert.Equal(2.00m, Money.Servicing(100m, 2m));
     }
+
+    [Fact]
+    public void PostsAtTheGivenRate()
+    {
+        Assert.Equal(4.00m, Money.Posting(100m, 4m));
+    }
 }
